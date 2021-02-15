115ec82eefe6e665db2bf5c41c9fa36c6c471f9c
Alexey <alexeyinshakov992@gmail.com>
2021-02-15 12:36:07 +0300

    movies v0.02

--- scripts.js
+++ scripts.js
@@ -14,6 +14,32 @@ let personalMovieDataBase = {
 
 for (let index = 0; index < 2; index++) {
     let film = prompt('Один из просмотренных фильмов?');
-    let rait = prompt('На сколько оцениваете его?');
-    personalMovieDataBase.movies[film] = rait;
+
+    if (film == "" || film == null || film.length > 50) {
+        index--;
+        continue;
+
+    } else {
+        for (let index = 0; index < 1; index++) {
+            let rait = prompt('На сколько оцениваете его?');
+            if (rait == "" || rait == null || rait.length > 50) {
+                index--;
+                continue;
+            } else {
+                personalMovieDataBase.movies[film] = rait;
+            }
+        }
+    }  
 }
+
+if (personalMovieDataBase.count < 10) {
+    console.log('Вы посмотрели довольно мало фильмов');
+} else if (personalMovieDataBase.count >= 10 && personalMovieDataBase.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDataBase.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDataBase);

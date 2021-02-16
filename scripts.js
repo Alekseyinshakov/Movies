eb9f5c718dd4c09ae3dec9455942c7f4684a0df0
Alexey <alexeyinshakov992@gmail.com>
2021-02-16 21:31:44 +0300

    Movies v0.03

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+let numberOfFilms;
+
+
+
+start();
 
 let personalMovieDataBase = {
     count: numberOfFilms,
@@ -12,34 +17,56 @@ let personalMovieDataBase = {
     privat: false
 };
 
-for (let index = 0; index < 2; index++) {
-    let film = prompt('Один из просмотренных фильмов?');
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB();
 
-    if (film == "" || film == null || film.length > 50) {
-        index--;
-        continue;
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-    } else {
-        for (let index = 0; index < 1; index++) {
-            let rait = prompt('На сколько оцениваете его?');
-            if (rait == "" || rait == null || rait.length > 50) {
-                index--;
-                continue;
-            } else {
-                personalMovieDataBase.movies[film] = rait;
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+    }
+}
+
+function rememberMyFilms() {
+    for (let index = 0; index < 2; index++) {
+        let film = prompt('Один из просмотренных фильмов?');
+
+        if (film == "" || film == null || film.length > 50) {
+            index--;
+            continue;
+
+        } else {
+            for (let index = 0; index < 1; index++) {
+                let rait = prompt('На сколько оцениваете его?');
+                if (rait == "" || rait == null || rait.length > 50) {
+                    index--;
+                    continue;
+                } else {
+                    personalMovieDataBase.movies[film] = rait;
+                }
             }
         }
-    }  
+    }
 }
 
-if (personalMovieDataBase.count < 10) {
-    console.log('Вы посмотрели довольно мало фильмов');
-} else if (personalMovieDataBase.count >= 10 && personalMovieDataBase.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDataBase.count > 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function detectPersonalLevel() {
+    if (personalMovieDataBase.count < 10) {
+        console.log('Вы посмотрели довольно мало фильмов');
+    } else if (personalMovieDataBase.count >= 10 && personalMovieDataBase.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDataBase.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
 
-console.log(personalMovieDataBase);
+function showMyDB(params) {
+    if (personalMovieDataBase.privat == false) {
+        console.log(personalMovieDataBase);
+    }
+}
+
+
